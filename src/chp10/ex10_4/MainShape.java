7e1c786733f6b240a590b526cd2f34b08aa076b1
wnddjgld <wnddjgld@gmail.com>
2024-10-17 09:46:29 +0900

    업캐스팅: 실습 10-4(배열) ArrayList

--- src/chp10/ex10_4/MainShape.java
+++ src/chp10/ex10_4/MainShape.java
@@ -1,5 +1,7 @@
 package chp10.ex10_4;
 
+import java.util.ArrayList;
+
 public class MainShape {
     public static void main(String[] args) {
         Square s = new Square();
@@ -9,9 +11,13 @@ public class MainShape {
         Circle c = new Circle();
         c.name = "원";
 
-        Shape[] shapes = {s, t, c};
-        for (int i = 0; i < shapes.length; i++) {
-            System.out.printf("%d번 인덱스의 도형: %s\n", i ,shapes[i].name);
+        ArrayList<Shape> shapes = new ArrayList<Shape>();
+        shapes.add(s);
+        shapes.add(t);
+        shapes.add(c);
+
+        for (int i = 0; i < shapes.size(); i++) {
+            System.out.printf("%d번 인덱스의 도형: %s\n", i ,shapes.get(i).name);
         }
     }
 }
