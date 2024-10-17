d9b539a3f942485ec0742916e250f6d99717099f
wnddjgld <wnddjgld@gmail.com>
2024-10-17 09:55:59 +0900

    업캐스팅: 실습 10-4 범위제한 실습

--- src/chp10/ex10_4/Square.java
+++ src/chp10/ex10_4/Square.java
@@ -1,4 +1,11 @@
 package chp10.ex10_4;
 
 public class Square extends Shape{
+    int pointNum = 4;
+
+    public double area(int width){
+        double area = Math.pow(width,2);
+
+        return area;
+    }
 }
